1208f0f9ff8a86973d29d979983d89c2e7b1cd42
TKCKWS <sara.aster+github@gmail.com>
2020-05-28 14:14:10 +0900

    カスタムバリデーションを利用したリクエストタイプチェック

--- SendMail/src/main/java/com/example/demo/sendmail/api/controller/SendMailController.java
+++ SendMail/src/main/java/com/example/demo/sendmail/api/controller/SendMailController.java
@@ -2,6 +2,7 @@ package com.example.demo.sendmail.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,48 +16,22 @@ public class SendMailController {
     @Autowired
     @Qualifier("SendMailServiceImpl")
     SendMailServiceImpl service;
-// 不要?サービス実装検討後削除
-//    @Autowired
-//    SendMailServiceCreateImpl createService;
-//
-//    @Autowired
-//    SendMailServiceUpdateImpl updateService;
-//
-//    @Autowired
-//    SendMailServiceCancelImpl cancelService;
 
     /**
      * メール送信用処理
      */
     @GetMapping("/sendmail")
-    public String sendMail(@ModelAttribute Request request) {
+    public String sendMail(@ModelAttribute @Validated Request request) {
         System.out.println(request);
 
         try {
-// 不要?サービス実装検討後削除
-//            SendMailService service = this.getService(request.getType());
+            System.out.println("service");
             service.sendMail(request);
         } catch (Exception e) {
+            System.out.println(e);
             return "NG";
         }
 
         return "OK";
     }
-
- // 不要?サービス実装検討後削除
-    /**
-     * リクエストパラメータに応じて、サービスインスタンスを分岐
-     */
-//    private SendMailService getService(String type) throws Exception {
-//        switch (type) {
-//        case "CREATE":
-//            return createService;
-//        case "UPDATE":
-//            return updateService;
-//        case "CANCEL":
-//            return cancelService;
-//        default:
-//            throw new Exception();
-//        }
-//    }
 }
